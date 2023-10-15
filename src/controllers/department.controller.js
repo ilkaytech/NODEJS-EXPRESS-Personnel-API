87c461efa22de17cb162a8b1d2dc4e249e14d512
ilkay <06ilkayozturk@gmail.com>
2023-10-15 20:34:46 +0300

    department.controller

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -2,3 +2,60 @@
 /* ---------------
     EXPRESS - Personnel API
 -------------------------------- */
+
+const Department = require("../models/department.model");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(Department);
+
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(Department),
+      data, // data:data
+    });
+  },
+
+  create: async (req, res) => {
+    const data = await Department.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+
+  read: async (req, res) => {
+    const data = await Department.findOne({ _id: req.params.id });
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  update: async (req, res) => {
+    const data = await Department.updateOne({ _id: req.params.id }, req.body);
+
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Department.findOne({ _id: req.params.id }),
+    });
+  },
+
+  delete: async (req, res) => {
+    const data = await Department.deleteOne({ _id: req.params.id });
+
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+    // const isDeleted = data.deletedCount >= 1 ? true : false
+
+    // res.status(isDeleted ? 204 : 404).send({
+    //     error: !isDeleted,
+    //     data
+    // })
+  },
+};
