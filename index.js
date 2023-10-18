3d355cf026d1107f4dbf185d3ad2e06b8607efc9
ilkay <06ilkayozturk@gmail.com>
2023-10-18 20:44:52 +0300

    logs

--- index.js
+++ index.js
@@ -26,6 +26,29 @@ dbConnection();
 /* ------------------------------------------------------- */
 // Middlewares:
 
+// Loogging:
+const morgan = require("morgan");
+// console.log(morgan);
+// app.use(morgan("tiny"));
+
+// //? write logs to file
+// const fs = require("node:fs");
+// app.use(
+//   morgan("combined", {
+//     stream: fs.createWriteStream("./accsess.log", { flags: "a" }),
+//   })
+// );
+
+//? write logs to file
+const fs = require("node:fs");
+const now = new Date();
+const today = now.toISOString().split("T")[0];
+app.use(
+  morgan("combined", {
+    stream: fs.createWriteStream(`./logs/${today}.log`, { flags: "a" }),
+  })
+);
+
 // Accept JSON:
 app.use(express.json());
 
