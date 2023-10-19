92b5ac821ccb1c3e797565d545ecfe43b3bafe38
ilkay <06ilkayozturk@gmail.com>
2023-10-19 16:37:56 +0300

    Swagger-UI Middleware

--- swagger.js
+++ swagger.js
@@ -6,19 +6,33 @@
 require("dotenv").config();
 const HOST = process.env?.HOST || "127.0.0.1";
 const PORT = process.env?.PORT || 8000;
-/* --------------------------------------------------- */
+/* ----------------------------------------------------*/
+
+/* ---------------------------------------------------- *
+const options = {
+	openapi:          <string>,     // Enable/Disable OpenAPI.                        By default is null
+	language:         <string>,     // Change response language.                      By default is 'en-US'
+	disableLogs:      <boolean>,    // Enable/Disable logs.                           By default is false
+	autoHeaders:      <boolean>,    // Enable/Disable automatic headers recognition.  By default is true
+	autoQuery:        <boolean>,    // Enable/Disable automatic query recognition.    By default is true
+	autoBody:         <boolean>,    // Enable/Disable automatic body recognition.     By default is true
+	writeOutputFile:  <boolean>     // Enable/Disable writing the output file.        By default is true
+};
+/* ------------------------------------------------------- */
+
+// const swaggerAutogen = require('swagger-autogen')({ openapi: '3.0.0' })
 const swaggerAutogen = require("swagger-autogen")();
 const packageJson = require("./package.json");
 
 const document = {
-  //   info: {
-  //     version: "1.0.0",
-  //     title: "Personnel API",
-  //     description: "Personnel Management API Service",
-  //     termsOfService: "http://www.clarusway.com",
-  //     contact: { name: "Clarusway", email: "qadir@clarusway.com" },
-  //     license: { name: "BSD License" },
-  //   },
+  // info: {
+  // 	version: "1.0.0",
+  // 	title: "Personnel API",
+  // 	description: "Personnel Management API Service",
+  // 	termsOfService: "http://www.ilkaytech.com",
+  // 	contact: { name: "Clarusway", email: "info@ilkaytech.com" },
+  // 	license: { name: "BSD License", },
+  // },
   info: {
     version: packageJson.version,
     title: packageJson.title,
@@ -27,7 +41,6 @@ const document = {
     contact: { name: packageJson.author, email: "info@ilkaytech.com" },
     license: { name: packageJson.license },
   },
-
   host: `${HOST}:${PORT}`,
   basePath: "/",
   schemes: ["http", "https"],
@@ -41,10 +54,38 @@ const document = {
         "Entry Your AccessToken (JWT) for Login. Example: <b>Bearer <i>...token...<i></b>",
     },
   },
+  security: [{ JWT: true }],
+  definition: {
+    "/auth/login": {
+      username: {
+        type: "String",
+        required: true,
+      },
+      password: {
+        type: "String",
+        required: true,
+      },
+    },
+    "/auth/refresh": {
+      "token.refresh": {
+        description: "{ token: { refresh: ... } }",
+        type: "String",
+        required: true,
+      },
+    },
+    // "Department": {
+    // 	"name": {
+    // 		type: "ObjectId",
+    // 		required: true
+    // 	}
+    // },
+    Department: require("./src/models/department.model").schema.obj,
+    Personnel: require("./src/models/personnel.model").schema.obj,
+  },
 };
 
 const routes = ["./index.js"];
 const outputFile = "./swagger.json";
 
-// Create JSON file
+// Create JSON file:
 swaggerAutogen(outputFile, routes, document);
